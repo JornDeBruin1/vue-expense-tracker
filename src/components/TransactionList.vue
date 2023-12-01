2f68c7096cd35c3577740bcfa7624a913aa6a6de
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-12-01 13:58:19 +0100

    Refactor components and add props

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,6 +1,11 @@
 <template>
     <h3>History</h3>
     <ul class="list" id="list">
+        <li v-for="transaction in transactions"
+         :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>€ {{ transaction.amount }}</span>
+            <button class="delete-btn">x</button>
+        </li>
         <!-- <li class="minus">
             Cash <span>-$400</span>
             <button class="delete-btn">x</button>
@@ -11,6 +16,14 @@
         </li> -->
     </ul>
 </template>
-<script>
 
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        transactions: {
+            type:Array,
+            required: true,
+        }
+    })
 </script>
