9b58fb175ae3e0850a9c5864ee1a9e89bb6b55f1
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-12-01 14:47:04 +0100

    Add vue-toastification package

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses"/>
-    <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses"/>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionDeleted" />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
@@ -15,20 +15,25 @@
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
 
-  import {ref, computed} from 'vue'
+  import{useToast} from 'vue-toastification';
 
-  const transactions = ref( 
-    [
-      {id: 1, text: 'Flower', amount: -19.99},
-      {id: 2, text: 'Salary', amount: 299.97},
-      {id: 3, text: 'Book', amount: -10},
-      {id: 4, text: 'Camera', amount: 150}
-    ]);
-    
+  import {ref, computed, onMounted} from 'vue';
+
+  const toast = useToast();
+
+  const transactions = ref([]);
+
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+    if(savedTransactions){
+      transactions.value = savedTransactions;
+    }
+  })
+
     //get total
     const total = computed(()=>{
       return transactions.value.reduce((acc, transaction) => {
-         return acc + transaction.amount
+         return acc + transaction.amount;
       },0);
     }); 
 
@@ -37,7 +42,7 @@
       return transactions.value
       .filter((transaction) => transaction.amount > 0)
       .reduce((acc, transaction) => {
-         return acc + transaction.amount
+         return acc + transaction.amount;
       },0).toFixed(2);
     }); 
 
@@ -46,7 +51,36 @@
       return transactions.value
       .filter((transaction) => transaction.amount < 0)
       .reduce((acc, transaction) => {
-         return acc + transaction.amount
+         return acc + transaction.amount;
       },0).toFixed(2);
     }); 
+
+    //add transaction
+    const handleTransactionSubmitted = (transactionData) => {
+         
+      transactions.value.push({
+        id: generateUniqueId(),
+        text: transactionData.text,
+        amount: transactionData.amount,
+      });
+      saveTransactionsToLocalStorage()
+      toast.success('Transaction added');
+    };
+
+    //generate unique id
+    const generateUniqueId = () => {
+      return Math.floor(Math.random() * 100000000);
+    };
+
+    //delete transaction
+    const handleTransactionDeleted = (id) => {
+      transactions.value = transactions.value.filter((transaction) => transaction.id !== id);
+      saveTransactionsToLocalStorage()
+      toast.success('Transaction deleted');
+    };
+
+    //save to localstorage
+    const saveTransactionsToLocalStorage = () =>{
+      localStorage.setItem('transactions',JSON.stringify(transactions.value))
+    }
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -4,21 +4,15 @@
         <li v-for="transaction in transactions"
          :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
             {{ transaction.text }} <span>€ {{ transaction.amount }}</span>
-            <button class="delete-btn">x</button>
+            <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
-        <!-- <li class="minus">
-            Cash <span>-$400</span>
-            <button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            paycheck <span>$1000</span>
-            <button class="delete-btn">x</button>
-        </li> -->
     </ul>
 </template>
 
 <script setup>
     import { defineProps } from 'vue';
+
+    const emit = defineEmits(['transactionDeleted'])
 
     const props = defineProps({
         transactions: {
@@ -26,4 +20,8 @@
             required: true,
         }
     })
+
+    const deleteTransaction = (id) => {
+        emit('transactionDeleted', id)
+    }
 </script>
