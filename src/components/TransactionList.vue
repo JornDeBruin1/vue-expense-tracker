45735326ef141010d18c505c360186ae152dc0f4
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-12-01 11:47:07 +0100

    content filled

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3>History</h3>
+    <ul class="list" id="list">
+        <!-- <li class="minus">
+            Cash <span>-$400</span>
+            <button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+            paycheck <span>$1000</span>
+            <button class="delete-btn">x</button>
+        </li> -->
+    </ul>
+</template>
+<script>
+
+</script>
