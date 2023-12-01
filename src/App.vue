7f3b261bc17cc130ab85de914966b97fc1fab9ff
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-12-01 14:05:48 +0100

    Update IncomeExpenses component to display actual income and expenses amounts

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <Header />
   <div class="container">
     <Balance :total="total" />
-    <IncomeExpenses />
+    <IncomeExpenses :income="income" :expenses="expenses"/>
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
@@ -19,16 +19,34 @@
 
   const transactions = ref( 
     [
-      {id: 1, text: 'Flower', amount: -20},
-      {id: 2, text: 'Salary', amount: 300},
+      {id: 1, text: 'Flower', amount: -19.99},
+      {id: 2, text: 'Salary', amount: 299.97},
       {id: 3, text: 'Book', amount: -10},
       {id: 4, text: 'Camera', amount: 150}
     ]);
+    
+    //get total
     const total = computed(()=>{
       return transactions.value.reduce((acc, transaction) => {
          return acc + transaction.amount
       },0);
     }); 
 
-    
+    //get income
+    const income = computed(()=>{
+      return transactions.value
+      .filter((transaction) => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+         return acc + transaction.amount
+      },0).toFixed(2);
+    }); 
+
+    //get expenses
+    const expenses = computed(()=>{
+      return transactions.value
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+         return acc + transaction.amount
+      },0).toFixed(2);
+    }); 
 </script>
