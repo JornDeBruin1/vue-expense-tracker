a7258115b1cfcd19a9c3414fdf340ce2b3ed2811
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-12-04 11:33:37 +0100

    Fix indentation in TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,7 @@
     <h3>History</h3>
     <ul class="list" id="list">
         <li v-for="transaction in transactions"
-         :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
             {{ transaction.text }} <span>€ {{ transaction.amount }}</span>
             <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
